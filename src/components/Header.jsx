f42f6f384ea0c6fb779608b30851f3e71f8eff9f
Omarespo-dev <omaresposito21@gmail.com>
2025-03-11 14:31:28 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+  return (
+    <header>
+        <h2>MOVIES</h2>
+    </header>
+  )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+
+export default function Main() {
+    return (
+        <main>
+            <div className="div-set">
+                <h2>MOVIES</h2>
+            </div>
+        </main>
+    )
+}
